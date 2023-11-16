bc11f5ba5727b81f401b39043856cf85afb49fcf
gabrielforms <gabrielformiga3@gmail.com>
2023-11-16 00:58:50 -0300

    Feat: Adição do modal ao clicar na receita

--- js/script.js
+++ js/script.js
@@ -43,9 +43,12 @@ $(document).ready(function () {
                 mealItem.classList.add('meal-item');
                 mealItem.dataset.id = value.id;
                 mealItem.innerHTML = `
-                <img src="${value.thumb}" alt="${value.name}">
+                <img src="${value.thumb}" alt="${value.name}" onclick="getMeal(${value.id})">
                 <h3>${value.name}</h3>
                 `;
+                mealItem.addEventListener('click', function() {
+                    getMeal(value.id);
+                });
                 mealList.appendChild(mealItem);
             })
         })
@@ -65,13 +68,52 @@ $(document).ready(function () {
                 mealItem.classList.add('meal-item');
                 mealItem.dataset.id = value.id;
                 mealItem.innerHTML = `
-                <img src="${value.thumb}" alt="${value.name}">
+                <img src="${value.thumb}" alt="${value.name}" onclick="getMeal(${value.id})">
                 <h3>${value.name}</h3>
                 `;
+                mealItem.addEventListener('click', function() {
+                    getMeal(value.id);
+                });
                 mealList.appendChild(mealItem);
             })
         })
+        $("body").on("click", "#recipeCloseBtn", function () {
+            document.querySelector(".modal-container").style.display = 'none';
+        })
     }
+
+    function getMeal(mealId) {
+        $.get("/api/main.php", {
+            action: 'get_recipe',
+            value: Number(mealId)
+        }, function (data, status) {
+            if (data.status == "error") {
+                alert(data.message);
+                return false;
+            }
+    
+            var mealDetailsContent = document.querySelector(".meal-details-content");
+            var modalContainer = document.querySelector(".modal-container");
+            var meal = data;
+    
+            mealDetailsContent.innerHTML = `
+                <h2 class="recipe-title">${meal.name}</h2>
+                <p class="recipe-category">${meal.category}</p>
+                <div class="recipe-instruct">
+                    <h3>Instruções:</h3>
+                    <p>${meal.instructions}</p>
+                </div>
+                <div class="recipe-img">
+                    <img src="${meal.thumb}" alt="${meal.name}">
+                </div>
+                <div class="recipe-video">
+                    
+                </div>
+            `;
+            modalContainer.style.display = 'block';
+        });
+    }
+
 
     loadDefault();
 })
